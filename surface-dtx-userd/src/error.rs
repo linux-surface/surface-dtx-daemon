406acc5269aa5f405e21ed9d7de60d113e7138bd
qzed <qzed@users.noreply.github.com>
2019-04-19 14:47:37 +0200

    Set-up error handling

---
+++ surface-dtx-userd/src/error.rs
@@ -0,0 +1,85 @@
+use failure::{Context, Fail, Backtrace};
+
+
+pub type Result<T> = std::result::Result<T, Error>;
+pub use failure::ResultExt;
+
+#[derive(Debug)]
+pub struct Error {
+    inner: Context<ErrorKind>,
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Fail)]
+pub enum ErrorKind {
+    #[fail(display = "Invalid configuration")]
+    Config,
+}
+
+
+impl Error {
+    pub fn kind(&self) -> ErrorKind {
+        *self.inner.get_context()
+    }
+
+    pub fn iter_causes(&self) -> failure::Causes {
+        ((&self.inner) as &dyn Fail).iter_causes()
+    }
+}
+
+impl From<ErrorKind> for Error {
+    fn from(kind: ErrorKind) -> Error {
+        Error { inner: Context::new(kind) }
+    }
+}
+
+impl From<Context<ErrorKind>> for Error {
+    fn from(inner: Context<ErrorKind>) -> Error {
+        Error { inner }
+    }
+}
+
+impl Fail for Error {
+    fn cause(&self) -> Option<&Fail> {
+        self.inner.cause()
+    }
+
+    fn backtrace(&self) -> Option<&Backtrace> {
+        self.inner.backtrace()
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        std::fmt::Display::fmt(&self.inner, f)
+    }
+}
+
+
+pub type CliResult = std::result::Result<(), CliError>;
+
+pub struct CliError {
+    error: Error,
+}
+
+impl From<Error> for CliError {
+    fn from(error: Error) -> Self {
+        CliError { error }
+    }
+}
+
+impl From<Context<ErrorKind>> for CliError {
+    fn from(error: Context<ErrorKind>) -> Self {
+        CliError { error: error.into() }
+    }
+}
+
+impl std::fmt::Debug for CliError {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(fmt, "{}.", self.error.kind())?;
+        for cause in self.error.iter_causes() {
+            write!(fmt, "\n       {}.", cause)?;
+        }
+
+        Ok(())
+    }
+}
--- surface-dtx-userd/src/main.rs
+++ surface-dtx-userd/src/main.rs
@@ -1,6 +1,11 @@
+mod error;
+use error::CliResult;
+
 mod cli;
 
 
-fn main() {
+fn main() -> CliResult {
     let matches = cli::app().get_matches();
+
+    Ok(())
 }
