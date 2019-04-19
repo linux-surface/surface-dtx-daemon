fdd2c357cc43f38c32a07dd33f4e4c5d081950f2
qzed <qzed@users.noreply.github.com>
2019-04-19 14:54:32 +0200

    Implement basic configuration

---
+++ surface-dtx-userd/src/config.rs
@@ -0,0 +1,81 @@
+use std::path::{Path, PathBuf};
+
+use serde;
+use serde::{Serialize, Deserialize};
+
+use crate::error::{Result, ResultExt, ErrorKind};
+
+
+const DEFAULT_CONFIG_PATH: &str = "/etc/surface-dtx/surface-dtx-userd.conf";
+
+
+#[derive(Debug, Serialize, Deserialize, Default, Clone)]
+pub struct Config {
+    #[serde(skip)]
+    pub dir: PathBuf,
+
+    #[serde(default)]
+    pub log: Log,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default, Clone)]
+pub struct Log {
+    #[serde(default)]
+    pub level: LogLevel,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
+#[serde(rename_all="lowercase")]
+pub enum LogLevel {
+    Critical,
+    Error,
+    Warning,
+    Info,
+    Debug,
+    Trace,
+}
+
+
+impl Config {
+    pub fn load() -> Result<Config> {
+        if Path::new(DEFAULT_CONFIG_PATH).exists() {
+            Config::load_file(DEFAULT_CONFIG_PATH)
+        } else {
+            Ok(Config::default())
+        }
+    }
+
+    pub fn load_file<P: AsRef<Path>>(path: P) -> Result<Config> {
+        use std::io::Read;
+
+        let mut buf = Vec::new();
+        let mut file = std::fs::File::open(path.as_ref()).context(ErrorKind::Config)?;
+        file.read_to_end(&mut buf).context(ErrorKind::Config)?;
+
+        let mut config: Config = toml::from_slice(&buf).context(ErrorKind::Config)?;
+        config.dir = path.as_ref().parent().unwrap().into();
+
+        Ok(config)
+    }
+}
+
+
+impl Default for LogLevel {
+    fn default() -> LogLevel {
+        LogLevel::Info
+    }
+}
+
+
+impl Into<slog::Level> for LogLevel {
+    fn into(self) -> slog::Level {
+        match self {
+            LogLevel::Critical => slog::Level::Critical,
+            LogLevel::Error    => slog::Level::Error,
+            LogLevel::Warning  => slog::Level::Warning,
+            LogLevel::Info     => slog::Level::Info,
+            LogLevel::Debug    => slog::Level::Debug,
+            LogLevel::Trace    => slog::Level::Trace,
+        }
+    }
+}
--- surface-dtx-userd/src/main.rs
+++ surface-dtx-userd/src/main.rs
@@ -3,9 +3,17 @@ use error::CliResult;
 
 mod cli;
 
+mod config;
+use config::Config;
+
 
 fn main() -> CliResult {
     let matches = cli::app().get_matches();
+
+    let config = match matches.value_of("config") {
+        Some(path) => Config::load_file(path)?,
+        None       => Config::load()?,
+    };
 
     Ok(())
 }
