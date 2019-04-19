277e1805408dd5462275e4f6e1ad0e9efc50b566
qzed <qzed@users.noreply.github.com>
2019-04-19 14:56:50 +0200

    Set-up logger

--- surface-dtx-userd/src/main.rs
+++ surface-dtx-userd/src/main.rs
@@ -6,6 +6,26 @@ mod cli;
 mod config;
 use config::Config;
 
+use slog::{Logger, o};
+
+
+fn logger(config: &Config) -> Logger {
+    use slog::{Drain};
+
+    let decorator = slog_term::TermDecorator::new()
+        .build();
+
+    let drain = slog_term::FullFormat::new(decorator)
+        .use_original_order()
+        .build()
+        .filter_level(config.log.level.into())
+        .fuse();
+
+    let drain = std::sync::Mutex::new(drain)
+        .fuse();
+
+    Logger::root(drain, o!())
+}
 
 fn main() -> CliResult {
     let matches = cli::app().get_matches();
@@ -14,6 +34,8 @@ fn main() -> CliResult {
         Some(path) => Config::load_file(path)?,
         None       => Config::load()?,
     };
+
+    let logger = logger(&config);
 
     Ok(())
 }
