ca747c194d0e98f56ae278a4d54992edc6c7a9e0
Maximilian Luz <luzmaximilian@gmail.com>
2021-04-06 21:27:45 +0200

    userd: Implement detach-cancel notifications

--- surface-dtx-userd/src/logic/core.rs
+++ surface-dtx-userd/src/logic/core.rs
@@ -1,6 +1,7 @@
 use crate::logic::{CancelReason, Event};
 use crate::utils::notify::{Notification, NotificationHandle, Timeout};
 
+use std::borrow::Cow;
 use std::sync::Arc;
 
 use anyhow::{Context, Result};
@@ -92,7 +93,85 @@ impl Core {
         // mark ourselves as canceled and prevent new detachment-ready notifications
         self.canceled = true;
 
-        // TODO: on error, show error notification
+        let (category, summary, body): (_, _, Cow<'static, str>) = match reason {
+            CancelReason::HandlerTimeout => (
+                "device.error",
+                "Surface DTX: Error",
+                "Detachment canceled due to handler timeout. \
+                 This may lead to data loss! \
+                 Please consult the logs for mode details."
+                    .into()
+            ),
+            CancelReason::Runtime(err) => match err {
+                super::types::RuntimeError::NotFeasible => (
+                    "device",
+                    "Surface DTX: Detachment canceled",
+                    "Detachment canceled by the controller. \
+                     Please make sure that the tablet battery is sufficently charged."
+                        .into()
+                ),
+                super::types::RuntimeError::Timeout => (
+                    "device.error",
+                    "Surface DTX: Detachment canceled",
+                    "The detachment process has timed out while the base was locked. \
+                     Please ensure that the detachment handler is set up correctly."
+                        .into()
+                ),
+                super::types::RuntimeError::Unknown(x) => (
+                    "device.error",
+                    "Surface DTX: Error",
+                    format!("Detachment canceled due to unknown runtime error ({}).", x)
+                        .into()
+                ),
+                _ => { return Ok(()); },
+            },
+            CancelReason::Hardware(err) => match err {
+                super::types::HardwareError::FailedToOpen => (
+                    "device.error",
+                    "Surface DTX: Error",
+                    "Hardware error: The controller failed to open the latch."
+                        .into()
+                ),
+                super::types::HardwareError::FailedToRemainOpen => (
+                    "device.error",
+                    "Surface DTX: Error",
+                    "Hardware error: The controller failed to keep the latch open."
+                        .into()
+                ),
+                super::types::HardwareError::FailedToClose => (
+                    "device.error",
+                    "Surface DTX: Error",
+                    "Hardware error: The controller failed to close the latch."
+                        .into()
+                ),
+                super::types::HardwareError::Unknown(x) => (
+                    "device.error",
+                    "Surface DTX: Error",
+                    format!("Detachment canceled due to unknown hardware error ({}).", x)
+                        .into()
+                ),
+            },
+            CancelReason::Unknown(x) => (
+                "device.error",
+                "Surface DTX: Error",
+                format!("Detachment canceled due to unknown error ({}).", x)
+                    .into()
+            ),
+            _ => { return Ok(()); },
+        };
+
+        let handle = Notification::create("Surface DTX")
+            .summary(summary)
+            .body(body)
+            .hint_s("image-path", "input-tablet")
+            .hint_s("category", category)
+            .hint("urgency", 2)
+            .build()
+            .show(&self.session).await
+            .context("Failed to display notification")?;
+
+        trace!(target: "sdtxu::notify", id = handle.id, ty = "detach-cancel",
+               "displaying notification");
 
         Ok(())
     }
