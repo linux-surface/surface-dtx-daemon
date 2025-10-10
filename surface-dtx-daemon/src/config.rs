5490cbcddf4ded11e2a240f93174ca73ac1352b5
Maximilian Luz <luzmaximilian@gmail.com>
2025-10-10 21:22:32 +0200

    Update dependencies

--- surface-dtx-daemon/src/config.rs
+++ surface-dtx-daemon/src/config.rs
@@ -103,7 +103,7 @@ impl Config {
         let data = std::str::from_utf8(&buf)
             .with_context(|| format!("Failed to read config file (path: {:?})", path.as_ref()))?;
 
-        let de = toml::Deserializer::new(data);
+        let de = toml::Deserializer::parse(data)?;
 
         let mut unknowns = BTreeSet::new();
         let mut config: Config = serde_ignored::deserialize(de, |path| {
